169d3a966bb70e9dbb013eacec5dc7e8fe7c47e4
Chenyin Liu <chenyin0126@gmail.com>
2018-12-15 23:58:45 -0800

    quicksort

--- src/main/java/com/svit/java/l4/quickSort.java
+++ src/main/java/com/svit/java/l4/quickSort.java
@@ -3,21 +3,46 @@ package com.svit.java.l4;
 public class quickSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] A = {2,6,1,0,89,87,43,21};
+		quickSort(A, 0, A.length-1);
+		
+		for(int i=0;i<A.length;i++) {
+			System.out.println(A[i]);
+		}
 
 	}
 	
-	public static void quickSort() {
+	public static void quickSort(int[] A, int start, int end) {
 		
+		if(start>=end) {
+			return;
+		}
 		
+		int index = start+(end-start)/2;
+		int pivot = A[index];
+		int left = start;
+		int right = end;
+		while(left<=right) {
+			
+			while(left<=right && A[left]<pivot ) {
+				left++;
+			}
+			
+			while(left<=right && A[right]>pivot) {
+				right--;
+			}
+			
+			if(left<=right) {
+				
+				int temp= A[left];
+				A[left] = A[right];
+				A[right] = temp;
+				left++;
+				right--;
+			}
+		}
 		
-		
-		
+		quickSort(A, start, right);
+		quickSort(A, left, end);
 	}
-	
-	
-	
-	
-	
-
 }
